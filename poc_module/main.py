6521b794dc8baa5f617312c838cb0139a8c2c6f5
mitzyvalenciaordonez <mitzyvo@gmail.com>
2025-06-06 12:15:28 -0500

    :sparkles: Added dependency

--- poc_module/main.py
+++ poc_module/main.py
@@ -1,1 +1,6 @@
-print('Hello')
+import flask
+import functions_framework
+
+@functions_framework.http
+def hello(request: flask.Request) -> flask.typing.ResponseReturnValue:
+    return "Hello world!"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(
-    name='poc-python-smvers',
-    version"1.0.0"
+    name="poc-python-smvers",
+    version="1.0.0"
 )
